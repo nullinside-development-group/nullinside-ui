b6c26de39d4609de150ede0affd3cd35b75b88f4
J█████ <dev.nullinside@gmail.com>
2024-02-12 18:12:17 -0500

    Updating device interface name

--- go.sh
+++ go.sh
@@ -1,4 +1,4 @@
-export IP_ADDRESS=$(/sbin/ip -o -4 addr list eno1 | awk '{print $4}' | cut -d/ -f1)
+export IP_ADDRESS=$(/sbin/ip -o -4 addr list eno2 | awk '{print $4}' | cut -d/ -f1)
 
 cp /secrets/certs/* nginx/filesystem/etc/nginx/certs
 docker build -t nullinside-ui:latest --build-arg BUILD_ENVIRONMENT=prod --build-arg IP_ADDRESS=$IP_ADDRESS .
