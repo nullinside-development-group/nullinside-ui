7661f9453cbf48c68d9940af632375f8dff3cf26
J█████ <dev.nullinside@gmail.com>
2024-02-28 18:10:09 -0500

    Logo: Fixing issue with logo click not working when not auth'd

--- src/src/app/common/components/logo/logo.component.ts
+++ src/src/app/common/components/logo/logo.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-logo',
@@ -17,6 +18,11 @@ export class LogoComponent {
   }
 
   sendToIndex() {
-    this.router.navigate(['/']);
+    // Need to use window.location here instead of the router because otherwise the external javascript from Google
+    // doesn't reload on the index page, and you can't retry your login until you refresh.
+    //
+    // @ts-expect-error: The expected usage of window.location is to set it directly as a string but due to typing
+    // issues that have changed over time the linting complains about it.
+    window.location = `${environment.siteUrl}`;
   }
 }
