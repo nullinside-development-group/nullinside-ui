96cdfee8bc1af56d6a045c24c18be1ce58636ab6
J█████ <dev.nullinside@gmail.com>
2024-05-22 20:56:19 -0400

    Updating to Angular v18

--- src/src/app/view/login-landing/login-landing.component.spec.ts
+++ src/src/app/view/login-landing/login-landing.component.spec.ts
@@ -1,8 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginLandingComponent } from './login-landing.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { RouterModule } from '@angular/router';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('GoogleSigninLandingComponent', () => {
   let component: LoginLandingComponent;
@@ -10,8 +11,9 @@ describe('GoogleSigninLandingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginLandingComponent, HttpClientTestingModule, RouterModule.forRoot([])]
-    })
+    imports: [LoginLandingComponent, RouterModule.forRoot([])],
+    providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
+})
       .compileComponents();
 
     fixture = TestBed.createComponent(LoginLandingComponent);
