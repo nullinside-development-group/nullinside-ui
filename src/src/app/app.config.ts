9e5a505fc5b6087fb2524bdd31ffa2ead978b22a
J█████ <dev.nullinside@gmail.com>
2026-01-19 19:08:25 -0500

    chore: update deps to fix build

--- src/src/app/app.config.ts
+++ src/src/app/app.config.ts
@@ -4,11 +4,11 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
-import {BearerTokenInterceptor} from "./middleware/bearer-token-interceptor";
+import {bearerTokenInterceptor} from "./middleware/bearer-token-interceptor";
 import {MAT_SNACK_BAR_DEFAULT_OPTIONS} from "@angular/material/snack-bar";
 
 export const noopInterceptorProvider: Provider =
-  {provide: HTTP_INTERCEPTORS, useClass: BearerTokenInterceptor, multi: true};
+  {provide: HTTP_INTERCEPTORS, useFactory: bearerTokenInterceptor, multi: true};
 
 export const materialSnackbarDefaults: Provider =
   {provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: {duration: 5 * 1000}};
