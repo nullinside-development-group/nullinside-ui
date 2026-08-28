7ae24848411b0b9a96b92b0fc9e4d0d7f0eba8eb
J█████ <dev.nullinside@gmail.com>
2026-08-27 21:39:00 -0400

    chore: code rework

--- src/src/app/view/twitch/twitch-bot-admin/twitch-bot-admin.ts
+++ src/src/app/view/twitch/twitch-bot-admin/twitch-bot-admin.ts
@@ -1,6 +1,5 @@
 import {
   Component,
-  computed,
   DestroyRef,
   ElementRef,
   inject,
@@ -31,7 +30,6 @@ import {TimestampPipe} from '../../../common/pipe/timestamp.pipe';
 export class TwitchBotAdmin implements OnInit {
   @ViewChild('chatConsole')
   chatConsole?: ElementRef<HTMLDivElement>;
-  private scrollChat = false;
 
   private timer?: number;
   private timerDestroy = inject(DestroyRef);
@@ -40,8 +38,7 @@ export class TwitchBotAdmin implements OnInit {
   protected loading = signal(true);
   private api: NullinsideTwitchBot = inject(NullinsideTwitchBot);
 
-  protected messages = signal<Record<string, TwitchChatMessage>>({});
-  protected messagesForDisplay = computed(() => Object.values(this.messages()));
+  protected messages = signal<TwitchChatMessage[]>([]);
 
   ngOnInit(): void {
     this.loadData();
@@ -80,49 +77,24 @@ export class TwitchBotAdmin implements OnInit {
     });
 
     this.api.getAllChatMessages().subscribe(response => {
-      const sortedMessages = response.data.slice().sort((a, b) => b.timestamp.getTime() - a.timestamp.getTime());
+      const sortedMessages = response.data
+        .slice()
+        .sort((a, b) => b.timestamp.getTime() - a.timestamp.getTime());
 
       const wasAtBottom = this.isAtTheBottomOfChat();
-      let collectionModified = false;
-      this.messages.update(currentMap => {
-        // Since this is a complex type and TypeScript compares them by reference, we need to check if the array has
-        // actually changed before interacting with the recentlyBanned collection. If we just update the array with the
-        // intended new values, the same exact rows will be considered different (by reference) and force an update
-        // every single cycle.
-        const updatedMap: Record<string, TwitchChatMessage> = {...currentMap};
 
-        sortedMessages.forEach(message => {
-          const key = message.id.toString();
+      this.messages.set(
+        sortedMessages.map(message => ({
+          id: message.id,
+          channel: message.channel,
+          sender: message.twitchUsername ?? 'Unknown',
+          message: message.message ?? 'Unknown',
+          timestamp: message.timestamp,
+          tooltip: `${message.twitchUsername} - ${message.message} [${convertForDisplay(message.timestamp)}]`
+        }))
+      );
 
-          // Only add if it's missing
-          if (!updatedMap[key]) {
-            updatedMap[key] = {
-              id: message.id,
-              channel: message.channel,
-              sender: message.twitchUsername ?? 'Unknown',
-              message: message.message ?? 'Unknown',
-              timestamp: message.timestamp,
-              tooltip: `${message.twitchUsername} - ${message.message} [${convertForDisplay(message.timestamp)}]`
-            };
-
-            collectionModified = true;
-          }
-        });
-
-        // Remove messages that are no longer in the chat list
-        Object.keys(updatedMap).forEach(key => {
-          if (!sortedMessages.find(b => b.id.toString() === key)) {
-            delete updatedMap[key];
-            collectionModified = true;
-          }
-        });
-
-        return updatedMap;
-      });
-
-      // Only scroll to the bottom if we were at the bottom before the collection was modified and the collection was
-      // modified.
-      if (wasAtBottom && collectionModified) {
+      if (wasAtBottom) {
         setTimeout(() => this.scrollToBottomOfChat());
       }
     });
